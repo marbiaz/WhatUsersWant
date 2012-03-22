6c94f82e93688d18d643b8b71eae1dcca7895e2b
Marco Biazzini <Marco.Biazzini@inria.fr>
2012-03-22 16:07:47 +0000

    bulk updates from old version

--- wuw/core/Config.java
+++ wuw/core/Config.java
@@ -5,10 +5,16 @@ package wuw.core;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.LinkedList;
+import java.util.Random;
 
 import wuw.comm.CommHandler;
 import wuw.comm.TCPProtocol;
 import wuw.comm.UDPProtocol;
+import wuw.comm.newscast.Newscast;
+import wuw.pi.PIHandler;
+import wuw.pi.PIStub;
+import wuw.ui.UIHandler;
+import wuw.ui.WebUIHandler;
 
 
 /**
@@ -21,11 +27,18 @@ import wuw.comm.UDPProtocol;
  */
 public class Config {
 
+public static final boolean printLogs = false;
+
 static final String usageString = "For a correct execution, the following arguments are needed, in the given order :\n"
     + "    -- The IPv4 address to be used by this instance of WUW.\n"
     + "    -- The port number to be used by this instance of WUW.\n"
     + "    -- The transport protocol to be used (either \"TCP\" or \"UDP\").\n"
     + "    -- The file that contains the list of neighbors for this instance of WUW.\n";
+
+/**
+ * Random number generator to be used by every object during an execution.
+ */
+static public Random rand = null;
 
 /**
  * This object represent the local peer.
@@ -34,24 +47,10 @@ static private Peer localPeer = null;
 
 
 /**
- * The peerList of neighbors known to the local peer.
- */
-static private PeerID[] peerList = null;
-
-
-/**
  * @return The local peer.
  */
 static public Peer getLocalPeer() {
   return localPeer;
-}
-
-
-/**
- * @return The peer list of neighbors.
- */
-static public PeerID[] getPeerList() {
-  return peerList;
 }
 
 
@@ -67,6 +66,11 @@ static public PeerID[] getPeerList() {
  */
 static public boolean set(String[] args) {
 
+  if (localPeer != null) {
+    System.err.println("CONFIG ERROR : config setting was attempted twice.");
+    return false;
+  }
+
   if (args.length < 4 // Just to catch some macroscopic mistyping...
       || !args[1].matches("[0-9]{4,5}")
       || !args[0].matches("[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}")
@@ -75,23 +79,37 @@ static public boolean set(String[] args) {
     return false;
   }
 
-  if (localPeer != null) {
-    System.err.println("CONFIG ERROR : config setting was attempted twice.");
-    return false;
-  }
-
+  rand = new Random(System.currentTimeMillis());
   int localPort = Integer.valueOf(args[1]);
   PeerID pid = new PeerID(args[0], localPort);
   CommHandler com = args[2].equalsIgnoreCase("TCP") ? new TCPProtocol(pid) : new UDPProtocol(pid);
+  Newscast news = new Newscast(null); // TODO: get parameters to newscast!
+  UIHandler ui = new WebUIHandler(); // TODO: write a decent configuration....
+  PIHandler pi = new PIStub(readPeerList(args[args.length - 1], true));
+  // TODO: write a decent pi configuration....
 
-  localPeer = new Peer(pid, com);
-  peerList = readPeerList(args[3], localPeer.getPeerID());
+  localPeer = new Peer(pid, com, ui, pi, news);
 
   return true;
 }
 
 
-static private PeerID[] readPeerList(String filePath, PeerID localID) {
+/**
+ * Very simple method that reads from a well-formatted file and builds a list of
+ * PeerIDs. It does not check for inconsistencies in the list, nor for duplicate
+ * entries. If called after {@link Config#set(String[])} has been called, can
+ * compare the PeerIDs in the provided list against the ID of the local peer,
+ * not to include it in the output.
+ * 
+ * @param filePath
+ *          The full path and file name where the list of peer must be read
+ *          from.
+ * @param shuffle
+ *          If true, the entries in the resulting array will be shuffled.
+ * @return An (possibly shuffled) array of PeerID read from the input file, or
+ *         <code>null</code> if some exception occurs.
+ */
+static public PeerID[] readPeerList(String filePath, boolean shuffle) {
   LinkedList<PeerID> pList = new LinkedList<PeerID>();
   BufferedReader buf;
   String line;
@@ -105,7 +123,7 @@ static private PeerID[] readPeerList(String filePath, PeerID localID) {
       tokens = line.split("[\\s]+");
       if (tokens[0].startsWith("peer.", 0)) {
         p = new PeerID(tokens[1], Integer.valueOf(tokens[2]));
-        if (!p.equals(localID)) {
+        if (localPeer == null || !p.equals(localPeer.localID)) {
           pList.add(p);
         }
       }
@@ -117,16 +135,47 @@ static private PeerID[] readPeerList(String filePath, PeerID localID) {
     e.printStackTrace();
     return null;
   }
+
   PeerID[] peerList = new PeerID[pList.size()];
   peerList = pList.toArray(peerList);
+/**/if (printLogs) System.out.println("Peers : " + printArray(peerList));
+  if (shuffle == true) peerList = shuffle(peerList, peerList.length);
 
   return peerList;
+}
+
+
+static private PeerID[] shuffle(PeerID[] orig, int size) {
+  PeerID[] res = new PeerID[size];
+  int[] temp = new int[orig.length];
+  int ran;
+  for (int i = 0; i < size;) {
+    ran = Config.rand.nextInt(orig.length);
+    if (temp[ran] == 0) {
+      temp[ran] = 1;
+      res[i] = orig[ran];
+      i++;
+    }
+  }
+  return res;
 }
 
 
 static private void usage() {
   System.err.println(usageString);
   return;
+}
+
+
+/**************************** DEBUG UTILITIES ********************************/
+
+static public String printArray(Object[] a) {
+  String res = "";
+  for (int i = 0; i < a.length; i++) {
+    res += a[i].toString() + " ";
+  }
+  res += "\n";
+  return res;
 }
 
 }
--- wuw/core/Peer.java
+++ wuw/core/Peer.java
@@ -2,7 +2,26 @@
 
 package wuw.core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.swing.Timer;
+
 import wuw.comm.CommHandler;
+import wuw.comm.newscast.Newscast;
+import wuw.pi.Transaction;
+import wuw.pi.PIHandler;
+import wuw.pi.Transaction.State;
+import wuw.pi.Transaction.Type;
+import wuw.ui.UIHandler;
 
 
 /**
@@ -13,18 +32,73 @@ import wuw.comm.CommHandler;
  */
 public class Peer {
 
+private boolean printLogs;
+
 final CommHandler transport;
+final UIHandler ui;
+final PIHandler pi;
+final Newscast epidemic;
 final PeerID localID;
+final ArrayList<ContentData> contents;
+final ArrayList<Neighbor> globalNeighborhood;
+BlockingQueue<PeerDescriptor> epidemicUpdates;
+// BlockingQueue<Transaction> p2pUpdates;
+Timer computer;
+
+public final boolean sharePrefs = true; // TODO: make these configurable!
+public final boolean shareInts = true;
+
+double pasSatisfaction = 0.5;
+double pacSatisfaction = 0.5;
+double pasAdequation = 0.5;
+double pacAdequation = 0.5;
+double pasSysEval = 1;
+double pacSysEval = 1;
 
 
 /*
- * Standard constructor. It should not be called but by the {@link Config#set(String[])} method.
+ * Standard constructor. It should not be called but by the {@link
+ * wuw.core.Config#set(String[])} method.
+ * 
  * @param p The {@link PeerID} of the local peer
+ * 
  * @param t The {@link CommHandler} object associated with the local peer
  */
-Peer(PeerID p, CommHandler t) {
+Peer(PeerID p, CommHandler t, UIHandler ui, PIHandler pi, Newscast n) {
   localID = p;
   transport = t;
+  epidemic = n;
+  this.ui = ui;
+  this.pi = pi;
+  contents = new ArrayList<ContentData>();
+  globalNeighborhood = new ArrayList<Neighbor>();
+  epidemicUpdates = new LinkedBlockingQueue<PeerDescriptor>(100);
+  // p2pUpdates = new LinkedBlockingQueue<Transaction>(100);
+  printLogs = Config.printLogs;
+
+  computer = new Timer(2000, new ActionListener() {
+
+    public void actionPerformed(ActionEvent e) {
+      doTheMagic();
+    }
+  });
+  computer.start();
+
+}
+
+
+public Object getDescriptor() {
+  // if (!(sharePrefs || shareInts)) return me; // ... what about c.ID & Co. ?
+  ContentData[] c = null;
+  int i = 0;
+  synchronized (contents) {
+    c = new ContentData[contents.size()];
+    Iterator<ContentData> cit = contents.iterator();
+    while (cit.hasNext()) {
+      c[i++] = cit.next();
+    }
+  }
+  return new PeerDescriptor(localID, System.currentTimeMillis(), c);
 }
 
 
@@ -41,6 +115,226 @@ public PeerID getPeerID() {
  */
 public CommHandler getTransport() {
   return transport;
+}
+
+
+/**
+ * @param id
+ *          The ID of the content to get.
+ * @return The {@link ContentData} identified by the given ID, or
+ *         <code>null</code>, if no such content is found.
+ */
+public ContentData getContent(String id) {
+  ContentData res = null;
+  synchronized (contents) {
+    int i = Collections.binarySearch(contents, new ContentData(id));
+    if (i >= 0) {
+      res = contents.get(i);
+    }
+  }
+  return res;
+}
+
+
+/**
+ * Add a content to the content list of the local peer. If a content with the
+ * same ID is already in the list, no addition occurs. Adding a content also
+ * adds the neighbors associated with the {@link ContentData} to local peers'
+ * neighborhood. The new neighbors, though, will not be taken into account to
+ * compute feedback values until the local peer gets to know some information
+ * about them (by means of either epidemic message exchange or local P2P
+ * application's data).
+ * 
+ * @param c
+ *          The content to add.
+ * @return <code>true</code> if the given content is added; <code>false</code>
+ *         if it was already in the list.
+ */
+public boolean addContent(ContentData c) {
+  PeerID remote;
+  int l = c.intentions.length;
+  int chosen = Config.rand.nextInt(l); // send at least 1 msg for sure!
+  synchronized (globalNeighborhood) { // to avoid releasing the lock (see
+                                      // addNeighbor) after each iteration...
+    for (int i = 0; i < l; i++) {
+      remote = c.intentions[i].remote;
+      addNeighbor(new Neighbor(remote, System.currentTimeMillis()),
+          (i == chosen || Config.rand.nextDouble() < (Math.log(l) / l)));
+      // TODO: add remote to the list to be given to P2P application!!!
+    }
+  }
+  synchronized (contents) {
+    int i = Collections.binarySearch(contents, c);
+    if (i < 0) {
+      contents.add(-i - 1, c);
+    }
+  }
+
+  return true;
+}
+
+
+void addNeighbor(Neighbor n, boolean sendCard) {
+  synchronized (globalNeighborhood) {
+    int index = Collections.binarySearch(globalNeighborhood, n);
+    if (index < 0) {
+      globalNeighborhood.add(-index - 1, n);
+    }
+  }
+  if (sendCard) {
+    this.epidemic.sendCard(n.ID);
+  }
+}
+
+
+/**
+ * Get the {@link PeerDescriptor}s updates from the overlay manager.
+ * 
+ * @param upd
+ *          Descriptors to get
+ */
+public void getEpidemicUpdates(Object upd[]) {
+  int i = 0;
+  boolean ok, clog = false;
+  while (i < upd.length) {
+    ok = epidemicUpdates.offer((PeerDescriptor)upd[i]);
+    if (ok)
+      i++;
+    else
+      clog = true;
+  }
+  if (clog) {
+/**/System.err.println("Peer: ATTENTION: Epidemic updates queue congestion!");
+    System.err.flush();
+  }
+/**/if (printLogs)
+    System.out.println("Peer : The epidemic updates queue currently contains "
+        + epidemicUpdates.size() + " descriptors.");
+}
+
+
+private Neighbor nLookUp(PeerID p) {
+  Neighbor n = null;
+  int i = Collections.binarySearch(globalNeighborhood, new Neighbor(p));
+  if (i >= 0) {
+    n = globalNeighborhood.get(i);
+  }
+  return n;
+}
+
+
+private ContentData cLookUp(String c) {
+  ContentData res = null;
+  int i = Collections.binarySearch(contents, new ContentData(c));
+  if (i >= 0) {
+    res = contents.get(i);
+  }
+  return res;
+}
+
+
+private void doTheMagic() {
+  // TODO: compute satisfaction and adequation and sysEvaluation.
+
+  ArrayList<PeerDescriptor> newDescriptors = new ArrayList<PeerDescriptor>();
+  epidemicUpdates.drainTo(newDescriptors);
+  Collections.sort(newDescriptors);
+
+  Transaction[] newTrans = pi.getContentUpdates();
+  // TODO : what if no transaction/descriptors??
+  Arrays.sort(newTrans);
+
+  PeerID id;
+  Neighbor n, m;
+  PeerDescriptor pd;
+  Transaction t;
+  Iterator<PeerDescriptor> pdit = newDescriptors.iterator();
+  HashMap<String, BitSet> cmap;
+  String cid, pcid;
+  BitSet newItems;
+  synchronized (globalNeighborhood) {
+    for (int i = 0; pdit.hasNext(); i++) {
+      n = globalNeighborhood.size() <= i ? null : globalNeighborhood.get(i);
+      pd = pdit.next();
+      if (n == null || (n.ID.compareTo(pd.ID) > 0)) {
+        // TODO: add new neighbor in position i
+        // TODO: keep track of the neighbor
+      } else if (n.ID.equals(pd.ID)) {
+        // TODO: update n with pd's data
+        // TODO: keep track of the neighbor
+      }
+    }
+
+/**//* if (printLogs) */System.out.println("New transactions :\n" + Config.printArray(newTrans));
+    newItems = new BitSet();
+    pcid = newTrans[0].getContentID();
+    m = null;
+    cmap = new HashMap<String, BitSet>();
+    for (int i = 0; i < newTrans.length; i++) {
+      t = newTrans[i];
+      id = t.getRemote();
+      cid = t.getContentID();
+      if (m != null && !id.equals(m.ID)) {
+        // TODO: add it to neighborhood
+        m = null;
+      }
+      n = nLookUp(id);
+      if (m == null && n == null) {
+        // TODO: create such a neighbor m
+      } else if (m == null) {
+        m = n;
+      }
+      // TODO: update m ...
+      // TODO: keep track of the neighbor, but only ONCE!!
+      if ((t.getType() == Type.IN) && (t.getState() == State.DONE)) {
+        if (!pcid.equals(cid)) {
+          if (!newItems.isEmpty()) {
+            if (cmap.containsKey(pcid)) {
+              cmap.get(pcid).or(newItems);
+            } else {
+              cmap.put(pcid, newItems);
+            }
+            newItems = new BitSet();
+          }
+          pcid = cid;
+        }
+        newItems.set(t.getItem());
+      }
+    }
+  }
+
+  ContentData cd;
+  synchronized (contents) { // ATTENTION : from now on, no modification to the
+                            // neighborhood!!!
+    Iterator<String> ci = cmap.keySet().iterator();
+    while (ci.hasNext()) {
+      cid = ci.next();
+      newItems = cmap.get(cid);
+      cd = cLookUp(cid);
+      if (cd == null) {
+        System.err
+            .println("Peer: UNRECOVERABLE ERROR : transaction received for inexistent content!");
+        System.err.flush();
+        System.exit(2); // FIXME: Think of something better...
+      }
+      cd.itemMap.or(newItems);
+    }
+
+    // TODO:  get new pref values from ui, if any.
+
+    // TODO:  compute new intentions per peer per content.
+  }
+
+/**//*if (printLogs) */System.out.println("Contents :\n" + Config.printArray(contents.toArray()));
+
+  // TODO:  send satisfaction and adequation and sysEvaluation to ui.
+
+  // TODO:  compute new ranked list of neighbors and give it to pi
+
+  // TODO:  optimizations:
+  //        apply epidemic updates to global neighborhood (track who is modified)
+  //        apply content updates to local contentData (track what is modified)
+  //        compute novel intentions for the MODIFIED neighbors/contents
 }
 
 }
