220519ce612defd7991d7f5881e7b255d41979d8
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2012-07-10 14:33:44 +0000

    New methods for sending peer list (ips-ports) to the BT local instance were added

--- wuw/pi/PIHandler.java
+++ wuw/pi/PIHandler.java
@@ -1,6 +1,7 @@
 package wuw.pi;
 
 import wuw.core.PeerID;
+import wuw.pi.BT.BtWuwPeer;
 
 
 /**
@@ -15,7 +16,7 @@ public interface PIHandler {
 
 /**
  * Retrieve information about contents download/upload status. This is the
- * way WUW gets to know the details about the status of the content trading activity.
+ * way WUW gets to know the details about the status of the content trading activity. 
  * 
  * @return The latest updates about the content trading on the local peer, or
  *         <code>null</code> if no update is available.
@@ -30,5 +31,23 @@ public Transaction[] giveContentUpdates();
  * @param peers The pere list for the given content.
  */
 public void getPeers(String contentID, PeerID[] peers);
+
+/**
+ * Get an array of the current best ranked peers according to the  
+ * {@code Peer.buildGlobalRanking()} method. This method is called by the tracker 
+ * emulator and the list of peers is sent to the local BitTorrent instance
+ * @return Array of the best ranked peers
+ * @author carvajal-r
+ */
+public BtWuwPeer[] getBestRankedPeers();
+
+/**
+ * Get an array of peers for answering the first announce message which has sent by the 
+ * local BitTorrent instance .This method is called by the tracker 
+ * emulator and the list of peers is sent to the local BitTorrent instance
+ * @return BitTorrent peer list for the first announce message
+ * @author carvajal-r
+ */
+public BtWuwPeer[] getPeersForAnnounce();
 
 }
