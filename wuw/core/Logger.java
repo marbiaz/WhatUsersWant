009c6623ffb3858eb7f42bf8031a4601b31c7a7e
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2012-07-10 14:17:24 +0000

    Comments and initialization values added

--- wuw/core/Logger.java
+++ wuw/core/Logger.java
@@ -4,50 +4,58 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * 
- * @author gomez-r
+ * Singleton object that lets records logs in a text file
+ * @author carvajal-r
  *
  */
 public class Logger {
-	
-	private static Logger instance = null;
-	private static FileWriter logFile = null;
-	private String actualLogLine = null;
-	
-	protected Logger(){
-		actualLogLine = "";
-		try {
-			logFile = new FileWriter(Config.getValue("localpeer", "logFile"), true);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
-	public static synchronized Logger getInstance(){
-		if(instance == null){
-			instance = new Logger();
-		}
-		return instance;
-	}
+private static Logger instance = null;
+private static FileWriter logFile = null;
+private String actualLogLine = null;
 
-	public void writeCurrentLogLine(){
-		try {
-			logFile.write(actualLogLine + "\n");
-			logFile.flush();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+protected Logger(String nameFile){
+  actualLogLine = "";
+  try {
+    logFile = new FileWriter(nameFile);
 
-	public void resetLogLine(){
-		actualLogLine = "";
-	}
+  } catch (IOException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }
+}
 
-	public void updateLogLine(String subStr){
-		actualLogLine = actualLogLine.concat(subStr);
-	}
+/**
+ * Gets the current instance of this object 
+ * @param nameFile Text file's full path 
+ * @return Instance of this object
+ */
+public static synchronized Logger getInstance(String nameFile){
+  if(instance == null){
+    instance = new Logger(nameFile);
+  }
+  return instance;
+}
+
+/**
+ * Write the current value of {@code actualLogLine} in the text file
+ */
+public void writeCurrentLogLine(){
+  try {
+    logFile.write(actualLogLine + "\n");
+    logFile.flush();
+  } catch (IOException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }
+}
+
+public void resetLogLine(){
+  actualLogLine = "";
+}
+
+public void updateLogLine(String subStr){
+  actualLogLine = actualLogLine.concat(subStr);
+}
 
 }
