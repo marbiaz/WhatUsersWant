4585a64dde76ec0a2c3807f98d15b4128bc1760a
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2013-01-08 10:41:58 +0000

    GPLheader update, WUW instead of Foobar

--- wuw/core/Neighbor.java
+++ wuw/core/Neighbor.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
  * PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with Foobar. 
+ * You should have received a copy of the GNU General Public License along with WUW. 
  * If not, see http://www.gnu.org/licenses/.
  */
 
